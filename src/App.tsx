c6e154a41b2e7976432aa5c3a34fead81f8f4a94
claitonbarreto <claitonbarreto@gmaiil.com>
2021-09-09 00:43:18 -0300

    style: 💄 create initial global style to application

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import GlobalStyle from './globalStyle';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
